727af4281aa20108042019feda41c8bff357aac9
Tushar <s.v.tushar04@gmail.com>
2022-03-27 09:58:00 +0530

    new img

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Homeabc from './components/Homepg';
 import Navbar from './components/Navbar';
 import SearchBar from './components/SearchBar';
 
